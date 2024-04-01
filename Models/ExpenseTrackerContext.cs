addd646b28c322217079727f34f1f57f4068f44d
pchandan <pchandan@BUSELABSH1906>
2024-04-01 15:49:29 -0600

    Add project files.

---
+++ Models/ExpenseTrackerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ExpenseTrackerContext:DbContext
+    {
+        public ExpenseTrackerContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
